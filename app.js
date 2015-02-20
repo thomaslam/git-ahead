c537efc06d22c919c914cf7f73689fd9aea01204
thomaslam <tuananhlam95@gmail.com>
2015-02-20 02:14:46 -0500

    add Procfile, .gitignore

--- app.js
+++ app.js
@@ -56,74 +56,74 @@
 //     });
 // });
 
+var port = process.env.PORT || 3000;
+
+
+
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+var server = app.listen(port, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+
+})
+
+module.exports = app;
 
 
 
 
-// var express = require('express')
-// var app = express()
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World!')
-// })
-
-// var server = app.listen(3000, function () {
-
-//   var host = server.address().address
-//   var port = server.address().port
-
-//   console.log('Example app listening at http://%s:%s', host, port)
-
-// })
-
-// module.exports = app;
 
 
+// var http = require('http'); // server library â€” built-in
+// var fs = require('fs');     // file system library â€” built-in
+// var mime = require('mime');
+// // Create a web server that, when a page request comes in, 
+// // e.g. "http://localhost:3000/index.html",
+// // passes it to a callback function to generate a response:
+// var server = http.createServer(
+//     function(request, response)
+//     {
+//         var filePath = './' + (request.url == '/' ? '/index.html' : request.url);
+//         // Check the file status:
+//         fs.exists(filePath, 
+//             function(exists)
+//             {
+//                 if (exists)
+//                     fs.readFile(filePath,
+//                         function(error, data)
+//                         {
+//                             if (error)
+//                             {
+//                                 response.writeHead(500, { 'Content-Type': 'text/plain' });
+//                                 response.end('Error 500: Internal Server Error');
+//                             }
+//                             else
+//                             {
+//                                 response.writeHead(200, 
+//                                     { 'Content-Type':  mime.lookup(filePath), 'Content-Length': data.length } 
+//                                 );
+//                                 response.end(data);
+//                             }
+//                         }
+//                     )
+//                 else
+//                 {
+//                     response.writeHead(404, { 'Content-Type': 'text/plain' });
+//                     response.end('Error 404: resource not found.');
+//                 }
+//             }
+//         );
+//     }
+// );
 
-
-
-
-var http = require('http'); // server library â€” built-in
-var fs = require('fs');     // file system library â€” built-in
-var mime = require('mime');
-// Create a web server that, when a page request comes in, 
-// e.g. "http://localhost:3000/index.html",
-// passes it to a callback function to generate a response:
-var server = http.createServer(
-    function(request, response)
-    {
-        var filePath = './' + (request.url == '/' ? '/index.html' : request.url);
-        // Check the file status:
-        fs.exists(filePath, 
-            function(exists)
-            {
-                if (exists)
-                    fs.readFile(filePath,
-                        function(error, data)
-                        {
-                            if (error)
-                            {
-                                response.writeHead(500, { 'Content-Type': 'text/plain' });
-                                response.end('Error 500: Internal Server Error');
-                            }
-                            else
-                            {
-                                response.writeHead(200, 
-                                    { 'Content-Type':  mime.lookup(filePath), 'Content-Length': data.length } 
-                                );
-                                response.end(data);
-                            }
-                        }
-                    )
-                else
-                {
-                    response.writeHead(404, { 'Content-Type': 'text/plain' });
-                    response.end('Error 404: resource not found.');
-                }
-            }
-        );
-    }
-);
-
-// Start up the web server:
-server.listen(3000, function() { console.log("Server listening on port 3000."); } );
+// // Start up the web server:
+// server.listen(3000, function() { console.log("Server listening on port 3000."); } );
